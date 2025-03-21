68cb90f3e9db79575399a77a175fcabbbc983304
onkr0d <90716666+onkr0d@users.noreply.github.com>
2025-03-21 17:22:42 -0400

    add functions:

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -16,7 +16,8 @@ const firebaseConfig = {
 };
 
 // Initialize Firebase
-const app = initializeApp(firebaseConfig);
+// const app = 
+initializeApp(firebaseConfig);
 
 const App = () => {
     return (

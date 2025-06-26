6cbb9216dd7c1827b23d14dbf6c5a6b052c6d22e
onkr0d <90716666+onkr0d@users.noreply.github.com>
2025-06-25 21:45:52 -0400

    chore: update Firebase configuration and workflows
    - Added frontend/.env to .gitignore.
    - Integrated SpicyPizza/create-envfile action in GitHub workflows for Firebase hosting.
    - Created a new workflow for deploying Firebase functions.
    - Updated index.html to enable Firebase App Check debug token in development.
    - Added axios dependency in package-lock.json.
    - Refactored Firebase initialization in App.tsx to use environment variables.
    - Adjusted API base URL in api.ts to use emulator in development.

--- frontend/functions/src/index.ts
+++ frontend/functions/src/index.ts
@@ -1,18 +1,6 @@
-/**
- * Import function triggers from their respective submodules:
- *
- * import {onCall} from "firebase-functions/v2/https";
- * import {onDocumentWritten} from "firebase-functions/v2/firestore";
- *
- * See a full list of supported triggers at https://firebase.google.com/docs/functions
- */
-
 import { beforeUserCreated} from "firebase-functions/v2/identity";
 import * as logger from "firebase-functions/logger";
 import { HttpsError } from "firebase-functions/v2/identity";
-
-// Start writing functions
-// https://firebase.google.com/docs/functions/typescript
 
 export const onlyAuthUsers = beforeUserCreated((event) => {
     const email = event.data?.email;

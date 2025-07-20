049bcee7066c28756a104f578cde244fe3dc6f6b
onkr0d <90716666+onkr0d@users.noreply.github.com>
2025-07-20 18:04:39 -0400

    feat: frontend sentry

---
+++ frontend/src/sentry.ts
@@ -0,0 +1,22 @@
+import * as Sentry from "@sentry/react";
+
+Sentry.init({
+  dsn: import.meta.env.VITE_SENTRY_DSN,
+  // Setting this option to true will send default PII data to Sentry.
+  // For example, automatic IP address collection on events
+  sendDefaultPii: true,
+  integrations: [Sentry.browserTracingIntegration()],
+
+  // Set tracesSampleRate to 1.0 to capture 100%
+  // of transactions for performance monitoring.
+  // We recommend adjusting this value in production
+  tracesSampleRate: 1.0,
+  // Set `tracePropagationTargets` to control for which URLs distributed tracing should be enabled
+  tracePropagationTargets: [
+    /^\//,                               // same-origin (relative) calls
+    /^https:\/\/compress\.ivan\.boston/, // prod API
+    /^https?:\/\/localhost:6969/,        // dev API  
+  ],
+  enabled: import.meta.env.PROD
+
+});
